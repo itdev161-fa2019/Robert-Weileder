741847aeacf58cd6aab07dc1c0e197ebac927464
Rob Weileder <weileder@gmatc.matc.edu>
2019-10-05 08:05:26 -0500

    reorganizing folders

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 import mongoose from 'mongoose';
 import config from 'config';
 
--- server.js
+++ server.js
@@ -25,25 +25,26 @@ app.get('/', (req, res) =>
  * @route POST api/users
  * @desc Register user
  */
-app.post('/api/users',
-[
-    check('name', 'Please enter your name')
-        .not()
-        .isEmpty(),
-    check('email', 'Please enter your email').isEmail(),
-    check(
-        'password',
-        'Please enter a password with 6 or more characters'
-        ).isLength({ min: 6 })
-],
- (req, res) => {
-     const errors = validationResult(req);
-     if (!errors.isEmpty()) {
-         return res.status(422).json({ errors: errors.array() });
-     } else {
-         return res.send(req.body);
-     }
-   }
+app.post(
+    '/api/users',
+    [
+        check('name', 'Please enter your name')
+            .not()
+            .isEmpty(),
+        check('email', 'Please enter your email').isEmail(),
+        check(
+            'password',
+            'Please enter a password with 6 or more characters'
+            ).isLength({ min: 6 })
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            return res.send(req.body);
+        }
+    }
 );
 
 // Connection listener
